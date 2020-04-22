88dd3b24a51050960405a85d1e1ce990c74b461d
Glitch (fast-likeable-huckleberry) <none>
2020-04-22 10:58:33 +0000

    🍖👶 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const app = express();
+
+let datas = ["Đi chợ","Nấu cơm","Rửa bát","Học code tại CodersX"]
+app.set('views', './views');
+app.set('view engine', 'pug');
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.send("I love CodersX");
+});
+
+app.get("/todos", (req, res) => {
+  let q = req.query.q;
+  if(q){
+    let filterData = datas.filter(item => item.toLowerCase().includes(q.toLowerCase()) === true);
+    res.render('index', { datas: filterData }); 
+  }
+   res.render('index', { datas }); 
+});
+
+// listen for requests :)
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
+});
